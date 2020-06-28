f3113da376f0768d975116ba49a7d7f84c562e71
Dima Vishnevetsky <dimshik100@gmail.com>
2020-06-28 16:40:17 +0300

    Added date type validation to Repeat class

--- src/classes/Repeat.js
+++ src/classes/Repeat.js
@@ -2,9 +2,11 @@ export class Repeat {
   constructor(data) {
     if (Array.isArray(data)) {
       this.days = data;
-    } else {
-      // TODO: Improve this state. Check if the accepted data is instance of Date and it's actually valid
+    } else if (data instanceof Date) {
+      // this is not checking if the date is valid. invalid dates will pass "new Date('random_string')"
       this.date = data;
+    } else {
+      throw new Error('Incorrect Repeat initial data');
     }
   }
 }
