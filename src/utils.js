e94dae5a32b412348b3f1e603ec186597d4e8a45
Shachar Oren <shacharorennn@gmail.com>
2020-07-22 17:50:55 +0300

    change gradientsArr colors

--- src/utils.js
+++ src/utils.js
@@ -18,20 +18,21 @@ export const getWeekDays = function(formatPattern = "iiiii") {
 };
 
 export const getColorByTime = function(hours, minutes, period) {
+  debugger;
   const gradientsArr = [
     {
       fromTime: {
-        hour: 12, // 1-12
+        hour: 0, // 1-12
         minute: 0, // 0-59
-        period: "PM" // AM/PM
+        period: "AM" // AM/PM
       },
       toTime: {
         hour: 4, // 1-12
         minute: 0, // 0-59
         period: "AM" // AM/PM
       },
-      fromColor: "#4a75e5",
-      toColor: "#3c1e84"
+      fromColor: "#7934f3",
+      toColor: "#6ea2f1"
     },
     {
       fromTime: {
@@ -41,20 +42,6 @@ export const getColorByTime = function(hours, minutes, period) {
       },
       toTime: {
         hour: 7, // 1-12
-        minute: 0, // 0-59
-        period: "AM" // AM/PM
-      },
-      fromColor: "#8c57e5",
-      toColor: "#eb95b7"
-    },
-    {
-      fromTime: {
-        hour: 7, // 1-12
-        minute: 1, // 0-59
-        period: "AM" // AM/PM
-      },
-      toTime: {
-        hour: 10, // 1-12
         minute: 0, // 0-59
         period: "AM" // AM/PM
       },
@@ -63,21 +50,35 @@ export const getColorByTime = function(hours, minutes, period) {
     },
     {
       fromTime: {
-        hour: 10, // 1-12
+        hour: 7, // 1-12
         minute: 1, // 0-59
         period: "AM" // AM/PM
       },
       toTime: {
-        hour: 4, // 1-12
+        hour: 10, // 1-12
         minute: 0, // 0-59
-        period: "PM" // AM/PM
+        period: "AM" // AM/PM
       },
       fromColor: "#8dfffd",
       toColor: "#1ea3ff"
     },
     {
       fromTime: {
-        hour: 4, // 1-12
+        hour: 10, // 1-12
+        minute: 1, // 0-59
+        period: "AM" // AM/PM
+      },
+      toTime: {
+        hour: 2, // 1-12
+        minute: 0, // 0-59
+        period: "PM" // AM/PM
+      },
+      fromColor: "#ffe4a8",
+      toColor: "#ff586e"
+    },
+    {
+      fromTime: {
+        hour: 2, // 1-12
         minute: 1, // 0-59
         period: "PM" // AM/PM
       },
@@ -119,29 +120,39 @@ export const getColorByTime = function(hours, minutes, period) {
     }
   ];
 
-  if (period === "PM" && hours < 12) {
-    hours = hours + 12;
-  }
+  // if (period === "PM" && hours < 12) {
+  //   console.log("PM");
+  //   hours = hours + 12;
+  // }
   if (period === "AM" && hours === 12) {
+    console.log("AM");
     hours = hours - 12;
   }
 
-  const minutesFromBeginningOfTheDay = getMinutesFromHourMinute(hours, minutes);
-
+  const minutesFromBeginningOfTheDay = getMinutesFromHourMinute(hours, minutes, period);
+  console.log("minutesFromBeginningOfTheDay" + minutesFromBeginningOfTheDay);
   let gradientData = gradientsArr[0]; // setting initial color
 
   for (const gradData of gradientsArr) {
-    const fromMinute = getMinutesFromHourMinute(gradData.fromTime.hour, gradData.fromTime.minute);
-    const toMinute = getMinutesFromHourMinute(gradData.toTime.hour, gradData.toTime.minute);
+    const fromMinute = getMinutesFromHourMinute(gradData.fromTime.hour, gradData.fromTime.minute, gradData.fromTime.period);
+    console.log("fromMinute" + fromMinute);
+    const toMinute = getMinutesFromHourMinute(gradData.toTime.hour, gradData.toTime.minute, gradData.toTime.period);
+    console.log("toMinute" + toMinute);
     if (minutesFromBeginningOfTheDay >= fromMinute && minutesFromBeginningOfTheDay <= toMinute) {
       gradientData = gradData;
       break;
     }
   }
+  console.log("-----------");
   console.log(gradientData);
   return gradientData;
 };
 
-export const getMinutesFromHourMinute = function(hours, minutes) {
-  return hours * 60 + minutes;
+export const getMinutesFromHourMinute = function(hours, minutes, period) {
+  if (period === "AM" || hours === 12) {
+    return hours * 60 + minutes;
+  } else { // "PM"
+    hours = hours + 12;
+    return hours * 60 + minutes;
+  }
 }
